ca2f21873f966a67535f1786cc998a1b2176ee73
Andrew Beatty <Andrew.Beatty@gmit.ie>
2019-11-21 15:57:38 +0000

    changed text

--- application.py
+++ application.py
@@ -8,7 +8,7 @@ app = Flask(__name__, static_url_path='', static_folder='.')
 
 @app.route('/')
 def index():
-    return "Hello, World!"
+    return "Hello, World! this is a change"
 
 @app.route('/book/<int:id>')
 def getBook(id):
