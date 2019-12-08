8e4a690f3410b831759ab33e95736d8bacfbcd26
andrewbeattycourseware <andrew.beatty@icloud.com>
2019-12-08 12:45:20 +0000

    fix db

--- bookDAO.py
+++ bookDAO.py
@@ -2,25 +2,35 @@ import mysql.connector
 import dbconfig as cfg
 class BookDAO:
     db=""
-    def __init__(self): 
+    def connectToDB(self):
         self.db = mysql.connector.connect(
-        host=       cfg.mysql['host'],
-        user=       cfg.mysql['user'],
-        password=   cfg.mysql['password'],
-        database=   cfg.mysql['database']
+            host=       cfg.mysql['host'],
+            user=       cfg.mysql['user'],
+            password=   cfg.mysql['password'],
+            database=   cfg.mysql['database']
         )
+    def __init__(self): 
+        connectToDB()
+     
+    
+    def getCursor(self):
+        if NOT self.db.is_connected():
+            connectToDB()
+        return self.db.cursor()
     
             
     def create(self, values):
-        cursor = self.db.cursor()
+        cursor = getCursor()
         sql="insert into book (title,author, price) values (%s,%s,%s)"
         cursor.execute(sql, values)
 
         self.db.commit()
-        return cursor.lastrowid
+        lastRowId=cursor.lastrowid
+        cursor.close
+        return lastRowId
 
     def getAll(self):
-        cursor = self.db.cursor()
+        cursor = getCursor()
         sql="select * from book"
         cursor.execute(sql)
         results = cursor.fetchall()
@@ -29,20 +39,22 @@ class BookDAO:
         for result in results:
             print(result)
             returnArray.append(self.convertToDictionary(result))
-
+        cursor.close
         return returnArray
 
     def findByID(self, id):
-        cursor = self.db.cursor()
+        cursor = getCursor()
         sql="select * from book where id = %s"
         values = (id,)
 
         cursor.execute(sql, values)
         result = cursor.fetchone()
-        return self.convertToDictionary(result)
+        book=self.convertToDictionary(result)
+        cursor.close()
+        return book
 
     def update(self, values):
-        cursor = self.db.cursor()
+        cursor = getCursor()
         sql="update book set title= %s,author=%s, price=%s  where id = %s"
         cursor.execute(sql, values)
         self.db.commit()
@@ -54,7 +66,8 @@ class BookDAO:
         cursor.execute(sql, values)
 
         self.db.commit()
-        print("delete done")
+        cursor.close()
+        
 
     def convertToDictionary(self, result):
         colnames=['id','Title','Author', "Price"]
